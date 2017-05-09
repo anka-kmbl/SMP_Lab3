87c31ee183f2c19cd028ef2dffcaac45795c1304
anka-kmbl <anka.kmbl@mail.ru>
2017-05-09 22:28:48 +0300

    Update addItem.php

--- addItem.php
+++ addItem.php
@@ -1,14 +1,22 @@
 <?php
 if(isset($_POST["crisps"]) || isset($_POST["Beer"]) || isset($_POST["cookies"]) || isset($_POST["cola"] )) {
 	session_start();
+	
+	$check = false;
 	foreach($_POST as $key => $value) {
 		if((int)$value > 0) {
 			$_SESSION[$key] = (int)$value;
-		} else {
+			$check = true;
+		} else if ((int)$value <= 0) {
 			header("Location: index.php?nothingChosen=1");
 			exit;
 		}
 	}
+	if(!$check) {
+		header("Location: index.php?nothingChosen=1");
+			exit;
+	}
+		
 }
 header("Location: basket.php");
 exit;

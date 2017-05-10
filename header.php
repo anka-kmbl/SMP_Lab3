89dbcd45e37df7213e08368ff4639fa596502178
anka-kmbl <anka.kmbl@mail.ru>
2017-05-10 16:34:45 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,16 @@
-
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style.css">
+<title>Shop</title>
+</head>
+<body>
+<div id="header">
+<ul id="navbar">
+<li><a href="#">Home</a></li>
+<li><a href="basket.php">Basket</a></li>
+</ul>
+</div>
+  </body>
+</html>

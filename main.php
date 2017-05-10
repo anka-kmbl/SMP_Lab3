8a361bb0260c320f7fdf5af3fbce2444516aa89d
anka-kmbl <anka.kmbl@mail.ru>
2017-05-10 16:36:34 +0300

    Create main.php

---
+++ main.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style.css">
+<title>Shop</title>
+</head>
+<body>
+
+<form action="addItem.php" method="post">
+<table>
+<tr>
+<th>
+Item
+</th>
+<th>
+Amount
+</th>
+<th>
+Price
+</th>
+
+</tr>
+<?php $prod = array("crisps" => 10, "Beer" => 15, "cookies" => 7, "cola" => 9); 
+?>
+<?php foreach ($prod as $key => $value) :?>
+<tr>
+<td><?php echo($key) ?></td>
+<td>
+<input style="width: 40px;" type="number" name="<?php echo($key) ?>">
+</td>
+<td><?php echo($prod[$key]." $") ?></td>
+</tr>
+
+<?php endforeach; ?>
+
+<tr>
+<td></td>
+<td></td>
+<td>
+<input type="submit" value="Buy" style="float: right;">
+</td>
+</tr>
+
+
+
+</table>
+
+<?php if(isset($_GET["nothingChosen"])): ?>
+	<h3>Check input, please!</h3>
+<?php endif; ?>
+</form>
+
+</body>
+</html>

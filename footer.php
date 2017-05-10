8702c62129ef28bcdd5f68d700cb40000ae384c8
anka-kmbl <anka.kmbl@mail.ru>
2017-05-10 16:35:32 +0300

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,1 +1,15 @@
-
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<link rel="stylesheet" href="style.css">
+<title>Shop</title>
+</head>
+<body>
+<div id="footer">
+<ul id="navF">
+<li><a href="#">Home</a></li>
+</ul>
+</div>
+</body>
+</html>

c6d8413e59e3c107d5c1a43d5afa2fd1b3dbad64
anka-kmbl <anka.kmbl@mail.ru>
2017-05-13 18:15:43 +0300

    header&footer modified

--- footer.php
+++ footer.php
@@ -1,15 +1,7 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta charset="utf-8">
-<link rel="stylesheet" href="style.css">
-<title>Shop</title>
-</head>
-<body>
+<?php
 <div id="footer">
 <ul id="navF">
 <li><a href="#">Home</a></li>
 </ul>
 </div>
-</body>
-</html>
+?>
--- header.php
+++ header.php
@@ -1,16 +1,10 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-<meta charset="utf-8">
-<link rel="stylesheet" href="style.css">
-<title>Shop</title>
-</head>
-<body>
+<?php
 <div id="header">
 <ul id="navbar">
 <li><a href="#">Home</a></li>
 <li><a href="basket.php">Basket</a></li>
 </ul>
 </div>
-  </body>
-</html>
+?>
+
+

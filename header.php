b5c48eb5870888d3b6fd8a9d51163d6e1f5e0980
anka-kmbl <anka.kmbl@mail.ru>
2017-05-14 00:42:21 +0300

    new files, js code ...

--- header.php
+++ header.php
@@ -6,7 +6,7 @@ echo '<div id="header">
 <li><a href="basket.php">Cart</a></li>
 <li><a href="register.php" id="login">Login</a></li>
 <li><a href="profile.php" id="profile">Profile</a></li>
-<li><a href="logout.php" id="logout">Logout</a></li>
+<li style="display:none;"><a href="logout.php" id="logout">Logout</a></li>
 </ul>
 </div>';
 ?>
